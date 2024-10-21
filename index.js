aff05a48b9f7bf54fada196662b53588ebeecbd0
João Victor Alves <joaovba2002@alu.ufc.br>
2024-10-21 09:47:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,401 @@
+// [DATA] dataset
+const nodes = [
+    { id: "A1" },
+    { id: "A2" },
+    { id: "A3" },
+    { id: "Q1" },
+    { id: "Q2" },
+    { id: "Q3" },
+    { id: "K1" },
+    { id: "K2" },
+    { id: "K3" }
+];
+
+const links = [
+    { source: "A1", target: "Q1", value: 1, qtd: null },
+    { source: "A1", target: "Q3", value: 2, qtd: null },
+    { source: "A2", target: "Q1", value: 3, qtd: null },
+    { source: "A2", target: "Q2", value: 3, qtd: null },
+    { source: "A2", target: "Q3", value: 3, qtd: null },
+    { source: "A3", target: "Q2", value: 2, qtd: null },
+    { source: "A3", target: "Q1", value: 1, qtd: null },
+    { source: "A3", target: "Q3", value: 2, qtd: null },
+    { source: "Q1", target: "K1", value: 1, qtd: 2 },
+    { source: "Q1", target: "K1", value: 3, qtd: 1 },
+    { source: "Q2", target: "K1", value: 2, qtd: 1 },
+    { source: "Q2", target: "K1", value: 3, qtd: 1 },
+    { source: "Q2", target: "K2", value: 2, qtd: 1 },
+    { source: "Q2", target: "K2", value: 3, qtd: 1 },
+    { source: "Q3", target: "K3", value: 2, qtd: 2 },
+    { source: "Q3", target: "K3", value: 3, qtd: 1 },
+];
+
+// [SETUP] width and height available
+const width = window.innerWidth - 35;
+const height = window.innerHeight - 20;
+const K = 50;
+const gap = 10;
+
+// [ALL FUNCTIONS]
+
+/**
+ * Creates a map of nodes where the key is the node id and the value is an object containing node details and links.
+ * @param {*} nodes - An array of nodes where each node is an object with an id property.
+ * @param {*} links - An array of links where each link is an object with a source and target property that contains the id of the source and target node.
+ * @returns {Object} A map of nodes where the key is the node id and the value is an object containing node details and links.
+ */
+function createNodeMap(nodes, links) {
+    const nodeMap = {};
+    nodes.forEach(node => {
+        nodeMap[node.id] = {
+            ...node,
+            sourceLinks: [],
+            targetLinks: []
+        };
+    });
+
+    links.forEach(link => {
+        const sourceNode = nodeMap[link.source];
+        const targetNode = nodeMap[link.target];
+
+        const linkWithNodes = {
+            ...link,
+            sourceNode: sourceNode,
+            targetNode: targetNode
+        };
+
+        sourceNode.sourceLinks.push(linkWithNodes);
+        targetNode.targetLinks.push(linkWithNodes);
+    });
+    return nodeMap;
+}
+
+/**
+ * Groups nodes by the initial character of their id.
+ *
+ * @param {Object} nodeMap - A map of nodes where the key is the node id and the value is an object containing node details and links.
+ * @returns {Object} An object where keys are the initial characters of node ids and values are arrays of nodes that share the same initial character.
+ */
+function groupNodesByInitial(nodeMap) {
+    const groups = {};
+
+    Object.values(nodeMap).forEach(node => {
+        const initial = node.id.charAt(0);
+
+        if (!groups[initial]) {
+            groups[initial] = [];
+        }
+
+        groups[initial].push(node);
+    });
+
+    return groups;
+}
+
+/**
+ * Calculates the height of a link based on its source node and a constant factor.
+ *
+ * @param {Object} link - The link object containing sourceNode and targetNode properties.
+ * @param {number} K - A constant factor used to calculate the height.
+ * @returns {number} The calculated height of the link.
+ */
+function heightLink(link, K) {
+    const sourceInitial = link.sourceNode.id[0];
+    let height;
+
+    if (sourceInitial == "A") {
+        height = K;
+    } else if (sourceInitial == "Q") {
+        const matchingLinks = link.sourceNode.targetLinks.filter(
+            l => l.targetNode.id == link.sourceNode.id && l.value == link.value
+        );
+        height = K * matchingLinks.length;
+    } else {
+        height = 5; // MIN HEIGHT
+    }
+
+    link.height = height;
+    return height;
+}
+
+/**
+ * Calculates and assigns the positions of nodes within the node map.
+ *
+ * @param {Object} nodeMap - A map of nodes where the key is the node id and the value is an object containing node details and links.
+ * @param {number} K - A constant factor used to calculate the height of nodes.
+ * @param {number} gap - The gap between nodes in the same group.
+ */
+function calculateNodePositions(nodeMap, K, gap) {
+    const nodeGroups = groupNodesByInitial(nodeMap);
+
+    Object.entries(nodeGroups).forEach(([key, nodes]) => {
+        let currentY = 0;
+
+        nodes.forEach(node => {
+            if (key == "A") {
+                // Grupo A: altura é 3 * K
+                node.height = 3 * K;
+            } else if (key == "Q") {
+                // Grupo Q: altura é quantidade de links de entrada * K
+                node.height = node.targetLinks.length * K;
+            } else if (key == "K") {
+                // Grupo K: altura é o somatório das alturas dos links de saída
+                node.height = node.targetLinks.reduce((sum, link) => {
+                    return sum + heightLink(link, K);
+                }, 0);
+            }
+
+            node.y = currentY;
+
+            currentY += node.height + gap;
+        });
+    });
+}
+
+/**
+ * Calculates and assigns the heights of links within the node map.
+ *
+ * @param {Object} nodeMap - A map of nodes where the key is the node id and the value is an object containing node details and links.
+ * @param {number} K - A constant factor used to calculate the height of links.
+ */
+function calculateLinkHeights(nodeMap, K) {
+    Object.values(nodeMap).forEach(node => {
+        node.targetLinks.forEach(link => {
+            link.height = heightLink(link, K);
+        });
+
+        node.sourceLinks.forEach(link => {
+            link.height = heightLink(link, K);
+        });
+    });
+}
+
+/**
+ * Defines the y0 position for links based on their source node and a factor.
+ *
+ * @param {Object} nodeMap - A map of nodes where the key is the node id and the value is an object containing node details and links.
+ * @param {number} K - A constant factor used to calculate the height of links.
+ * @param {number} factor - A factor used to adjust the y0 position of links.
+ */
+function defineY0ForLinks(nodeMap, links, K, factor) {
+    const nodeGroups = groupNodesByInitial(nodeMap); // Agrupar nós por inicial (A, Q, K)
+    Object.entries(nodeGroups).forEach(([key, nodes]) => {
+        if (key == "A") {
+            nodes.forEach(node => {
+                const sortedLinks = node.sourceLinks.sort((a, b) => a.value - b.value);
+                let yOffset = node.y + factor;
+
+                [1, 2, 3].forEach(value => {
+                    const linksOfValue = sortedLinks.filter(link => link.value == value);
+                    linksOfValue.forEach(link => {
+                        link.y0 = yOffset; // Atualizar y0 do link
+
+                        // Atualizar targetLinks com a mesma referência
+                        link.sourceNode.sourceLinks.forEach(targetLink => {
+                            if (targetLink == link) {
+                                targetLink.y0 = yOffset; // Manter a consistência
+                            }
+                        });
+                    });
+
+                    if (value == 1) {
+                        yOffset += 0;
+                    } else {
+                        yOffset += K;
+                    }
+                });
+            });
+        } else if (key == "Q") {
+            nodes.forEach(node => {
+                const sortedLinks = node.sourceLinks.sort((a, b) => {
+                    if (a.qtd !== b.qtd) {
+                        return a.qtd - b.qtd;
+                    }
+                    return a.value - b.value;
+                });
+
+                let currentY = node.y + factor;
+
+                sortedLinks.forEach(link => {
+                    const originalLink = links.find(l => l.id == link.id)
+                    link.y0 = currentY;
+                    // Atualizar targetLinks com a mesma referência
+                    link.sourceNode.sourceLinks.forEach(targetLink => {
+                        if (targetLink == link) {
+                            targetLink.y0 = currentY; // Manter a consistência
+                        }
+                    });
+
+                    currentY += link.height;
+                });
+            });
+        }
+    });
+}
+
+
+/**
+ * This function processes a map of nodes and applies a given factor to each node.
+ * 
+ * @param {Object} nodeMap - An object where keys are node identifiers and values are node objects.
+ * @param {number} factor - A numerical factor that will be applied to each node in the nodeMap.
+ */
+function defineY1ForLinks(nodeMap, link, K, factor) {
+    const nodeGroups = groupNodesByInitial(nodeMap); // Agrupar nós por inicial (A, Q, K)
+
+    Object.entries(nodeGroups).forEach(([key, nodes]) => {
+        if (key == "Q") {
+            nodes.forEach(node => {
+                const sortedLinks = node.targetLinks.sort((a, b) => a.value - b.value);
+                let currentY1 = node.y + factor;
+
+                sortedLinks.forEach(link => {
+                    link.y1 = currentY1;
+
+                    link.targetNode.targetLinks.forEach(sourceLink => {
+                        if (sourceLink == link) {
+                            sourceLink.y1 = currentY1;
+                        }
+                    });
+
+                    currentY1 += link.height;
+                });
+            });
+        } else if (key == "K") {
+            nodes.forEach(node => {
+                const sortedLinks = node.targetLinks.sort((a, b) => a.value - b.value);
+                let currentY1 = node.y + factor;
+
+                sortedLinks.forEach(link => {
+                    const originalLink = links.find(l => l.id == link.id);
+
+                    link.y1 = currentY1;
+                    originalLink.y1 = currentY1;
+
+                    link.targetNode.targetLinks.forEach(sourceLink => {
+                        if (sourceLink == link) {
+                            sourceLink.y1 = currentY1;
+                        }
+                    });
+
+                    currentY1 += link.height;
+                });
+            });
+        }
+    });
+}
+
+/**
+ * This function syncronize links based on nodeMap
+ * @param {*} nodeMap - An object where keys are node identifiers and values are node objects.
+ * @param {*} links - A list of original links
+ */
+function syncLinkPositions(nodeMap, links) {
+    Object.values(nodeMap).forEach(node => {
+        node.sourceLinks.forEach(sourceLink => {
+            const originalLink = links.find(link =>
+                link.source == sourceLink.sourceNode.id && link.target == sourceLink.targetNode.id && sourceLink.value == link.value
+            );
+            if (originalLink) {
+                if (sourceLink.x0 !== undefined) originalLink.x0 = sourceLink.x0;
+                if (sourceLink.x1 !== undefined) originalLink.x1 = sourceLink.x1;
+                if (sourceLink.y0 !== undefined) originalLink.y0 = sourceLink.y0;
+                if (sourceLink.y1 !== undefined) originalLink.y1 = sourceLink.y1;
+                if (sourceLink.height !== undefined) originalLink.height = sourceLink.height;
+            }
+        });
+
+        node.targetLinks.forEach(targetLink => {
+            const originalLink = links.find(link =>
+                link.source == targetLink.sourceNode.id && link.target == targetLink.targetNode.id && targetLink.value == link.value
+            );
+            if (originalLink) {
+                if (targetLink.x0 !== undefined) originalLink.x0 = targetLink.x0;
+                if (targetLink.x1 !== undefined) originalLink.x1 = targetLink.x1;
+                if (targetLink.y0 !== undefined) originalLink.y0 = targetLink.y0;
+                if (targetLink.y1 !== undefined) originalLink.y1 = targetLink.y1;
+                if (targetLink.height !== undefined) originalLink.height = targetLink.height;
+            }
+        });
+    });
+}
+
+
+// [MAP] map nodes and links
+const nodeMap = createNodeMap(nodes, links);
+
+// [MAP] define x position of node groups
+const nodeGroups = groupNodesByInitial(nodeMap);
+const nodeGroupKeys = Object.keys(nodeGroups);
+const nodeWidth = 25;
+
+const groupCount = nodeGroupKeys.length;
+const groupSpacing = (width - nodeWidth) / (groupCount - 1);
+
+nodeGroupKeys.forEach((key, index) => {
+    let xPosition;
+    if (index == 0) {
+        xPosition = 0;
+    } else if (index == groupCount - 1) {
+        xPosition = width - nodeWidth;
+    } else {
+        xPosition = index * groupSpacing;
+    }
+    nodeGroups[key].forEach(node => {
+        node.x = xPosition;
+    });
+});
+
+// [MAP] map height of nodes
+calculateNodePositions(nodeMap, K, gap);
+
+// [MAP] map links height
+calculateLinkHeights(nodeMap, K);
+// [MAP] Define x0 and x1
+Object.values(nodeMap).forEach(node => {
+    node.targetLinks.forEach(link => {
+        link.x0 = link.sourceNode.x;
+        link.x1 = link.targetNode.x;
+    });
+});
+// [MAP] Define y0 (out) and y1 (in)
+defineY0ForLinks(nodeMap, links, K, 0);
+defineY1ForLinks(nodeMap, links, K, 0);
+syncLinkPositions(nodeMap, links);
+
+console.log(nodeMap)
+console.log(links)
+
+// [DRAW] create svg
+const svg = d3.select("#sankey")
+    .append("svg")
+    .style("border", "1px solid black")
+    .attr("width", width)
+    .attr("height", height);
+
+// [DRAW] create nodes Vs
+const Vs = svg.selectAll(".node")
+    .data(Object.values(nodeMap))
+    .enter()
+    .append("rect")
+    .attr("class", "node")
+    .attr("x", d => {
+        return d.x
+    })
+    .attr("y", d => d.y)
+    .attr("width", nodeWidth)
+    .attr("height", d => d.height)
+    .style("fill", "steelblue");
+
+const As = svg.selectAll(".link")
+    .data(links)
+    .enter()
+    .append("line")
+    .attr("class", "link")
+    .attr("x1", d => d.x0 + nodeWidth)
+    .attr("y1", d => d.y0)
+    .attr("x2", d => d.x1)
+    .attr("y2", d => d.y1)
+    .style("stroke", "black")
+    .style("stroke-width", 1);
+
